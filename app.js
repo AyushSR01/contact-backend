843dbb8afa2efe55174927a1c1a9c369c8a09ced
AyushSR01 <you@example.com>
2026-01-03 11:45:14 +0530

    update db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/contactsdb')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/contacts_db', )
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error('MongoDB Error:', err));
 
