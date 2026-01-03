e79d86d6bee87795de34ff9fe8ccd0e820f4d248
AyushSR01 <you@example.com>
2026-01-03 11:50:30 +0530

    ok

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.CORS_ORIGIN || '*',
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 app.use(express.json());
